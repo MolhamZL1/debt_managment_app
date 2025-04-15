24780e3b1593e7bf257f684274c9b57b90e9ea56
molhamsheikh <molhamsa49@gmail.com>
2025-04-15 17:03:25 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DebtManagmentApp());
+}
+
+class DebtManagmentApp extends StatelessWidget {
+  const DebtManagmentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
